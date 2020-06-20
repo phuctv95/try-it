b45f33d8c3a51c5822fc8b97f22efb39ee988a6b
phuc <phuctv95@gmail.com>
2020-06-20 16:35:04 +0700

    dotnet: add ML.NET

---
+++ dotnet/TryMachineLearning/TryMachineLearning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TryMachineLearningML.Model;
+
+namespace TryMachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput
+            {
+                Vendor_id = "CMT",
+                Rate_code = 1,
+                Passenger_count = 1,
+                Trip_distance = 3.8f,
+                Payment_type = "CRD"
+            };
+            var result = ConsumeModel.Predict(input);
+            Console.WriteLine(result.Score);
+        }
+    }
+}
