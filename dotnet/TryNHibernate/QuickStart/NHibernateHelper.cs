239ade0aabf8423a7303413b7093316021b567bc
phuc <phuctv95@gmail.com>
2020-10-12 00:59:40 +0700

    NHibernate: add Quick Start

---
+++ dotnet/TryNHibernate/QuickStart/NHibernateHelper.cs
@@ -0,0 +1,32 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+
+namespace QuickStart
+{
+    public sealed class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    _sessionFactory = new Configuration().Configure().BuildSessionFactory();
+                }
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+        public static void CloseSession()
+        {
+            SessionFactory.Close();
+        }
+    }
+}
---
+++ dotnet/TryNHibernate/QuickStart/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace QuickStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var session = NHibernateHelper.OpenSession();
+            try
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    //var princess = new Cat
+                    //{
+                    //    Name = "Princess",
+                    //    Sex = 'F',
+                    //    Weight = 7.4f
+                    //};
+
+                    //session.Save(princess);
+                    //tx.Commit();
+
+                    var females = session
+                        .Query<Cat>()
+                        .Where(c => c.Sex == 'F')
+                        .ToList();
+                    foreach (var cat in females)
+                    {
+                        Console.WriteLine($"Female Cat: {cat.Name} ({cat.Id})");
+                    }
+                }
+            }
+            finally
+            {
+                NHibernateHelper.CloseSession();
+            }
+        }
+    }
+}
