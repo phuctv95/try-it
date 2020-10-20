184b1d815f3778f026c1aad65d699d59181958e0
Tran Van Phuc <prv@elca.ch>
2020-10-20 16:04:08 +0700

    WPF: add a custom dependency property

--- dotnet/TryWpf/TryWpf/MainWindow.xaml.cs
+++ dotnet/TryWpf/TryWpf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TryWpf
 {
@@ -20,9 +21,23 @@ namespace TryWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty CounterProperty =
+            DependencyProperty.Register(nameof(Counter), typeof(int), typeof(MainWindow));
+        public int Counter
+        {
+            get { return (int)GetValue(CounterProperty); }
+            set { SetValue(CounterProperty, value); }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Counter = 0;
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (_, __) => Counter++;
+            timer.Start();
         }
     }
 }
