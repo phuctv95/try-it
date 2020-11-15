aae0608cfd7a2da436faf17dc17af25d2affe058
phuc <phuctv95@gmail.com>
2020-11-15 16:49:54 +0700

    dotnet: add Expression100

---
+++ dotnet/TryConsole/Tests/HelperTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+
+namespace TryConsole.Tests
+{
+    [TestClass()]
+    public class HelperTests
+    {
+        [TestMethod()]
+        public void Expression100Test()
+        {
+            var computer = new DataTable();
+            foreach (var expression in Helper.Expression100())
+            {
+                var actual = computer.Compute(expression, string.Empty);
+                Assert.AreEqual(100, actual);
+            }
+        }
+    }
+}
---
+++ dotnet/TryConsole/TryConsole/Helper.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace TryConsole
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Requirement:
+        ///     Write a program that outputs all possibilities to put + or - or nothing
+        ///     between the numbers 1, 2, …, 9 (in this order) such that the result is
+        ///     always 100. For example: 1 + 2 + 34 – 5 + 67 – 8 + 9 = 100.
+        /// Solution:
+        ///     Brute force method.
+        /// </summary>
+        public static IList<string> Expression100()
+        {
+            var operators = new string[] { "+", "-", string.Empty };
+            var computer = new DataTable();
+            var result = new List<string>();
+            foreach (var o1 in operators)
+            {
+                foreach (var o2 in operators)
+                {
+                    foreach (var o3 in operators)
+                    {
+                        foreach (var o4 in operators)
+                        {
+                            foreach (var o5 in operators)
+                            {
+                                foreach (var o6 in operators)
+                                {
+                                    foreach (var o7 in operators)
+                                    {
+                                        foreach (var o8 in operators)
+                                        {
+                                            var expression = $"1{o1}2{o2}3{o3}4{o4}5{o5}6{o6}7{o7}8{o8}9";
+                                            if ((int)computer.Compute(expression, string.Empty) == 100)
+                                            {
+                                                result.Add(expression);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
