11ee54b89e7f6a469cb4f251daa2bd921a8f8497
phuc <phuctv95@gmail.com>
2020-12-01 22:23:26 +0700

    dotnet: gRPC - add unit test

---
+++ dotnet/TryGrpc/Tests/Services/GreeterServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using NUnit.Framework;
+using TryGrpc;
+
+namespace Tests.Services
+{
+    class GreeterServiceTests
+    {
+        private const string Address = "https://localhost:5001";
+
+        [Test]
+        public void Test1()
+        {
+            const string Name = "World";
+            var channel = GrpcChannel.ForAddress(Address);
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = client.SayHello(new HelloRequest { Name = Name });
+
+            Assert.AreEqual($"Hello {Name}", response.Message);
+        }
+    }
+}
