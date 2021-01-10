57ae4b1e300590eac1b599428b2e4548d1c95590
phuc <phuctv95@gmail.com>
2021-01-10 10:47:32 +0700

    dotnet: update unit tests

--- dotnet/TryDependencyInjection/Tests/BookConsoleTest.cs
+++ dotnet/TryDependencyInjection/Tests/BookConsoleTest.cs
@@ -14,23 +14,75 @@ namespace TryDependencyInjection
         private const int ActionExit = 4;
         private IBookRepository _bookRepository = Substitute.For<IBookRepository>();
         private IMyConsole _console = Substitute.For<IMyConsole>();
+        private Book _book { get; } = new Book
+        {
+            Id = Guid.NewGuid(),
+            Title = "Some title",
+            Available = true,
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bookRepository.GetAllBooks().Returns(new List<Book>() { _book });
+        }
 
         [Test]
         public void Run_OnStartup_ShowAllBooks()
         {
             var bookConsole = new BookConsole(_bookRepository, _console);
-            var book = new Book
-            {
-                Id = Guid.NewGuid(),
-                Title = "Some title",
-                Available = true,
-            };
-            _bookRepository.GetAllBooks().Returns(new List<Book>() { book });
             _console.ReadLine().Returns(ActionExit.ToString());
 
             bookConsole.Run();
 
-            _console.Received(1).WriteLine($"0 | {book.Title} | {(book.Available ? "Is available" : "Not available")}");
+            _console.Received(1).WriteLine($"0 | {_book.Title} | {(_book.Available ? "Is available" : "Not available")}");
+        }
+
+        [Test]
+        public void Run_WhenUserChooseInsert_CallInsertBook()
+        {
+            const string NewBookTitle = "Some Title";
+            var bookConsole = new BookConsole(_bookRepository, _console);
+            _console.ReadLine().Returns(
+                ActionInsert.ToString(),
+                NewBookTitle,
+                ActionExit.ToString());
+
+            bookConsole.Run();
+
+            _bookRepository.Received(1).Insert(Arg.Is<Book>(b => b.Id == Guid.Empty && b.Title == NewBookTitle));
+        }
+
+        [Test]
+        public void Run_WhenUserChooseUpdate_CallUpdateBook()
+        {
+            const string NewBookTitle = "Some Title";
+            var bookConsole = new BookConsole(_bookRepository, _console);
+            _console.ReadLine().Returns(
+                ActionUpdate.ToString(),
+                0.ToString(),
+                NewBookTitle,
+                ActionExit.ToString());
+
+            bookConsole.Run();
+
+            _bookRepository.Received(1).Update(Arg.Is<Book>(
+                b => b.Id == _book.Id && b.Title == NewBookTitle && b.Available == _book.Available));
+        }
+
+        [Test]
+        public void Run_WhenUserChooseRemove_CallRemoveBook()
+        {
+            var bookConsole = new BookConsole(_bookRepository, _console);
+            _console.ReadLine().Returns(
+                ActionRemove.ToString(),
+                0.ToString(),
+                ActionExit.ToString());
+
+            bookConsole.Run();
+
+            _bookRepository.Received(1).Remove(Arg.Is<Book>(
+                b => b.Id == _book.Id && b.Title == _book.Title && b.Available == _book.Available));
         }
     }
 }
--- dotnet/TryDependencyInjection/Tests/BookCsvRepositoryTest.cs
+++ dotnet/TryDependencyInjection/Tests/BookCsvRepositoryTest.cs
@@ -34,10 +34,103 @@ namespace Tests
             }
             finally
             {
-                if (File.Exists(BookFileName))
+                File.Delete(BookFileName);
+            }
+        }
+
+        [Test]
+        public void Insert()
+        {
+            try
+            {
+                var book = new Book
                 {
-                    File.Delete(BookFileName);
-                }
+                    Title = "Some title",
+                    Available = false,
+                };
+
+                _bookRepository.Insert(book);
+
+                var lines = File.ReadAllText(BookFileName)
+                    .Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                lines.Length.Should().Be(1);
+                var rowData = lines[0].Split(CsvSeparator);
+                (Guid.Parse(rowData[0])).Should().NotBeEmpty();
+                rowData[1].Should().Be(book.Title);
+                (bool.Parse(rowData[2])).Should().Be(book.Available);
+            }
+            finally
+            {
+                File.Delete(BookFileName);
+            }
+        }
+
+        [Test]
+        public void Update()
+        {
+            try
+            {
+                var book = new Book
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Some title",
+                    Available = false,
+                };
+                File.WriteAllText(BookFileName, $"{book.Id}{CsvSeparator}{book.Title}{CsvSeparator}{book.Available}");
+                _bookRepository.GetAllBooks();
+
+                book.Title = "New title";
+                _bookRepository.Update(book);
+
+                var lines = File.ReadAllText(BookFileName)
+                    .Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                lines.Length.Should().Be(1);
+                var rowData = lines[0].Split(CsvSeparator);
+                (Guid.Parse(rowData[0])).Should().Be(book.Id);
+                rowData[1].Should().Be(book.Title);
+                (bool.Parse(rowData[2])).Should().Be(book.Available);
+            }
+            finally
+            {
+                File.Delete(BookFileName);
+            }
+        }
+
+        [Test]
+        public void Remove()
+        {
+            try
+            {
+                var book1 = new Book
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Title 1",
+                    Available = false,
+                };
+                var book2 = new Book
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Title 2",
+                    Available = true,
+                };
+                File.WriteAllText(BookFileName,
+                    $"{book1.Id}{CsvSeparator}{book1.Title}{CsvSeparator}{book1.Available}{Environment.NewLine}"
+                    + $"{book2.Id}{CsvSeparator}{book2.Title}{CsvSeparator}{book2.Available}");
+                _bookRepository.GetAllBooks();
+
+                _bookRepository.Remove(book1);
+
+                var lines = File.ReadAllText(BookFileName)
+                    .Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                lines.Length.Should().Be(1);
+                var rowData = lines[0].Split(CsvSeparator);
+                (Guid.Parse(rowData[0])).Should().Be(book2.Id);
+                rowData[1].Should().Be(book2.Title);
+                (bool.Parse(rowData[2])).Should().Be(book2.Available);
+            }
+            finally
+            {
+                File.Delete(BookFileName);
             }
         }
     }
