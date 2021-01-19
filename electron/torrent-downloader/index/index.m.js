4b571f7d7c250a3227659f3b614ace495f0b8d91
Tran Van Phuc <prv@elca.ch>
2021-01-19 11:32:42 +0700

    electron: torrent - update

--- electron/torrent-downloader/index/index.m.js
+++ electron/torrent-downloader/index/index.m.js
@@ -48,6 +48,8 @@ ipcMain.on(channels.DownloadTorrent, (event, magnetUrl, selectedFolder) => {
 });
 
 ipcMain.on(channels.OpenSelectFolderDialog, (_, __) => {
-    let selectedFolder = dialog.showOpenDialogSync({properties: ['openDirectory']})[0];
-    win.webContents.send(channels.OnSelectedFolder, selectedFolder);
+    let selected = dialog.showOpenDialogSync({properties: ['openDirectory']});
+    if (!selected) { return; }
+
+    win.webContents.send(channels.OnSelectedFolder, selected[0]);
 });
