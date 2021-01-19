dd6cc36e0ed1383132e71d73ea9b7214410718ea
Tran Van Phuc <prv@elca.ch>
2021-01-19 10:29:09 +0700

    electron: torrent - update

--- electron/torrent-downloader/index/index.m.js
+++ electron/torrent-downloader/index/index.m.js
@@ -13,7 +13,7 @@ let win;
 function createWindow() {
     win = new BrowserWindow({
         width: 800,
-        height: 600,
+        height: 650,
         webPreferences: {
             nodeIntegration: true
         }
