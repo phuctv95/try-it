a2701657cc0ca07c1e481f284294c9504f003220
Tran Van Phuc <prv@elca.vn>
2021-02-27 18:15:11 +0700

    android: update try-android

--- android/try-android/app/src/main/java/com/example/tryandroid/MainActivity.kt
+++ android/try-android/app/src/main/java/com/example/tryandroid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tryandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
 import android.widget.EditText
 
@@ -12,14 +13,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        editText.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+                sendMessage()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
     }
 
-    fun sendMessage(view: View) {
+    private fun sendMessage() {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
         }
         startActivity(intent)
+    }
+
+    fun sendMessage(view: View) {
+        sendMessage()
     }
 }
